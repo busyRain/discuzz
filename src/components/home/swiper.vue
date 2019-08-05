9cf08620ecaf8ce558326cba620b9ded11c18511
tanww <kathy_tan1988@126.com>
2019-08-05 20:05:45 +0800

    bbs轮播图,y轴滚动

--- src/components/home/swiper.vue
+++ src/components/home/swiper.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <el-carousel height="320px">
+	<div>
+		<el-carousel height="320px">
 			<el-carousel-item v-for="(swiperItem, index) in swiperList" :key="index">
 				<a :href="swiperItem.topicurl || 'javascript:;'" target="__blank" class="carousel-item-container">
 					<el-image :src="$IMG_URL + swiperItem.imgurl">
@@ -11,8 +11,8 @@
 				</a>
 			</el-carousel-item>
 		</el-carousel>
-  </div>
-    <!-- <div class="swiper-wrapper">
+	</div>
+	<!-- <div class="swiper-wrapper">
     <swiper :options="swiperOption">
       <swiper-slide v-for="(swiperItem, index) in swiperList" :key="index">
         <a :href="swiperItem.topicurl || 'javascript:;'" target="__blank">
@@ -21,66 +21,74 @@
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
     </swiper> -->
-  </div>
+	</div>
 </template>
 <script>
-import 'swiper/dist/css/swiper.css';
-import { swiper, swiperSlide } from 'vue-awesome-swiper';
-import * as api from '@/api/home'
-export default {
-  name: "swiperWrapper",
-  components: {
-    swiper,
-    swiperSlide
-  },
-  data() {
-    return {
-      swiperOption: {
-          pagination: {
-            el: '.swiper-pagination',
-            bulletClass: 'bullet',
-            bulletActiveClass: 'bullet-active'
-          },
-          autoplay: true,
-          delay: 1500
-        },
-      swiperImg: '/img/home/banner/banner1.png',
-      swiperList: [
-        
-      ]
-    };
-  },
-  methods: {
-    async getSwiperList() {
-      await api.getSwiperList().then(res=>{
-        const {data} =res
-        if(res.code ==0){
-          this.swiperList=data
-        }
-      })
-    }
-  },
-  mounted() {
-    this.getSwiperList();
-  }
-};
+	import 'swiper/dist/css/swiper.css';
+	import {
+		swiper,
+		swiperSlide
+	} from 'vue-awesome-swiper';
+	import * as api from '@/api/home'
+	export default {
+		name: "swiperWrapper",
+		components: {
+			swiper,
+			swiperSlide
+		},
+		data() {
+			return {
+				swiperOption: {
+					pagination: {
+						el: '.swiper-pagination',
+						bulletClass: 'bullet',
+						bulletActiveClass: 'bullet-active'
+					},
+					autoplay: true,
+					delay: 1500
+				},
+				swiperImg: '/img/home/banner/banner1.png',
+				swiperList: [
+
+				]
+			};
+		},
+		methods: {
+			async getSwiperList() {
+				await api.getSwiperList().then(res => {
+					const {
+						data
+					} = res
+					if (res.code == 0) {
+						this.swiperList = data
+					}
+				})
+			}
+		},
+		mounted() {
+			this.getSwiperList();
+		}
+	};
 </script>
 <style lang="scss" scoped>
-// .swiper-wrapper {
-//   /* height: 400px; */
-//   border-radius: 5px;
-//   overflow: hidden;
-// }
-// .swiper-img {
-//   width: 550px;
-//   height: 320px;
-//   border-radius: 5px;
-//   display: block;
-// }
-// img {
-//   background-color: #fff;
-// }
-.carousel-item-container {
+	// .swiper-wrapper {
+	//   /* height: 400px; */
+	//   border-radius: 5px;
+	//   overflow: hidden;
+	// }
+	// .swiper-img {
+	//   width: 550px;
+	//   height: 320px;
+	//   border-radius: 5px;
+	//   display: block;
+	// }
+	// img {
+	//   background-color: #fff;
+	// }
+	.el-carousel--horizontal{
+		overflow-y: hidden;
+	}
+	.carousel-item-container {
 		width: 100%;
 		display: block;
 		height: 100%;
@@ -101,14 +109,16 @@ export default {
 	.el-carousel__indicators {
 		.el-carousel__indicator {
 			.el-carousel__button {
-				background-color: rgba(255,255, 255, 0.9); 
+				background-color: rgba(255, 255, 255, 0.9);
 			}
 		}
 	}
-	.el-carousel__arrow{
-		background-color: rgba(124,124,124,.8);
+
+	.el-carousel__arrow {
+		background-color: rgba(124, 124, 124, .8);
 	}
-	.el-carousel__arrow:hover{
-		background-color: rgba(31,45,61,0.8);
+
+	.el-carousel__arrow:hover {
+		background-color: rgba(31, 45, 61, 0.8);
 	}
 </style>
