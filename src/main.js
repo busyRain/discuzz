b6050b1c5acd164cac57d02810bb89525fb0e04c
tanww <kathy_tan1988@126.com>
2019-08-09 11:14:12 +0800

    https

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV == 'development') {
 } else if (process.env.NODE_ENV == 'production') {
 	Vue.prototype.$IMG_URL = 'http://images.feileyuan.com/';
 	Vue.prototype.$BBS_URL = 'http://www.feileyuan.com/';
-	Vue.prototype.$BBS_domain = 'https://bbs.feileyuan.com/';
+	Vue.prototype.$BBS_domain = 'http://bbs.feileyuan.com/';
 }
 Vue.prototype.$setCookie = SetCookies;
 Vue.prototype.$getCookie = GetCookies;
